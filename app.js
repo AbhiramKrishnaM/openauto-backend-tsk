6402f1c52051694937c3a0b5a37a54cb98e02053
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2023-02-19 02:00:34 +0530

    added env file and env file example

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log("Server running at port ", port);
+});
