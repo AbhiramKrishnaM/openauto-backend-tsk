2806ee2e48d405600a6b2972befac02ae0f106cc
AbhiramKrishnaM <abhiramKrishna.8921@gmail.com>
2023-02-19 12:03:16 +0530

    added route use for submit

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 
+// route imports
+const user = require("./router/user.js");
+
 const express = require("express");
 
 const app = express();
@@ -7,14 +10,17 @@ const app = express();
 const bodyParser = require("body-parser");
 
 // db init
-
 const { startSequelize } = require("./config/db.js");
 
+// use body parser
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// initializing PG module
 startSequelize();
+
+app.use("/submit", user);
 
 const port = process.env.APP_PORT || 4001;
 
--- controller/user.js
+++ controller/user.js
@@ -1,3 +1,7 @@
-function submitForm(req, res) {}
+function submitForm(req, res) {
+  console.log(req.body);
+
+  res.send("Hey recieved the body");
+}
 
 module.exports = { submitForm };
